ae2cb6e2fc119b9dcc46a071e460cba9ef4b962d
max <ren.maxime@gmail.com>
2020-03-21 18:05:27 +0100

    "checkstyle, pmd, refactoring and adding logs for exception"

--- src/main/java/fr/houseofcode/dap/commandLine/rma/CmdLineLauncher.java
+++ src/main/java/fr/houseofcode/dap/commandLine/rma/CmdLineLauncher.java
@@ -1,8 +1,6 @@
 package fr.houseofcode.dap.commandLine.rma;
 
-import java.awt.Desktop;
 import java.io.IOException;
-import java.net.URI;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,87 +10,75 @@ import org.apache.logging.log4j.Logger;
  * 5 august 2019.
  */
 
-public class CmdLineLauncher {
+public final class CmdLineLauncher {
 
     /**
      * @return access to constant LOG.
      */
-    private static  Logger LOG = LogManager.getLogger();
+    private static final Logger LOG = LogManager.getLogger();
 
-    //    private  static String USER_AGENT = "Mozilla/5.0";
+    /**
+     * Constructor private of CmdLineLauncher.
+     */
+    private CmdLineLauncher() {
+    }
 
     /**
      * @param args for method main
      * @throws IOException exception
      */
-    public static void main(String[] args) throws IOException {
-
-        LOG.debug("D�but du Main avec comme argument : " + args);
-
-        
-            
-            UtilsServer server = new UtilsServer();
-            
-        	 final int nUMBERtHREE = 3;
-             final int nUMBERnINE = 9;
-
-             int value = Integer.parseInt(args[0]);
-             String userKey = args[1];
-             
-            
-
-            LOG.info("Utilisation d'un switch pour indiquer"
-                    + " a l'utilisateur les possibilit�s" + args);
-            switch (value) {
+    public static void main(final String[] args) throws IOException {
+        LOG.debug("Début du Main avec comme argument : {}.", args);
+        UtilsServer server = new UtilsServer();
+        LOG.info("Instanciation d'un UtilsServer");
+        final int numberThree = 3;
+        final int numberFive = 5;
+        final int numberHeight = 8;
+        final int numberNine = 9;
+        Integer value = Integer.parseInt(args[0]);
+        String userKey = args[1];
+        LOG.info("Utilisation d'un switch pour indiquer a l'utilisateur les possibilités : {}.", args);
+        switch (value) {
             case 1:
                 LOG.info("Recuperation des labels GMAIL");
-                
                 System.out.println(server.getLabels(userKey));
                 break;
-
             case 2:
                 LOG.info("Recuperation du nombre d'emails Gmail");
-                
                 System.out.println("vous avez "
-                        + server.getNbUnreadEmail(userKey)
-                        + " message non lus");
+                    + server.getNbUnreadEmail(userKey)
+                    + " message non lus");
                 break;
-
-            case nUMBERtHREE:
+            case numberThree:
                 LOG.info("Recuperation du prochain "
-                        + "evenements inscrit dans l'API Calendar");
-                
+                    + "evenements inscrit dans l'API Calendar");
                 System.out.println(server.getNextEvent(userKey));
                 break;
-
-            case nUMBERnINE:
+            case numberFive:
                 LOG.info("Recuperation en un seul"
-                        + " appel de tous les �l�ments sus-mentionn�s");
-                
+                    + " appel de tous les éléments"
+                    + " sus-mentionnés");
                 System.out.println(server.getLabels(userKey));
                 System.out.println("vous avez "
-                + server.getNbUnreadEmail(userKey) + " message non lus");
+                    + server.getNbUnreadEmail(userKey)
+                    + " message non lus");
                 System.out.println(server.getNextEvent(userKey));
                 break;
-                
-            case 5:
-            	 LOG.info("creation d'un compte");
-            	server.createAccount(userKey);
-
-            default:
-                LOG.warn("Message par d�faut retourne"
-                        + " en cas de non utilisation des variables"
-                        + " 1, 2, 3 ou 9");
-                System.out.println("vous devez rentrer un numero"
-                        + " : 1, 2, 3 ou 9");
+            case numberHeight:
+                LOG.info("Ajout d'un user");
+                server.addUser(userKey);
                 break;
-
-            }
-        
-
+            case numberNine:
+                LOG.info("creation d'un compte");
+                server.createAccount(userKey);
+                break;
+            default:
+                LOG.warn("Message par défaut retourne"
+                    + " en cas de non utilisation des variables"
+                    + " 1, 2, 3 ou 9");
+                System.out.println("vous devez rentrer un numero"
+                    + " : 1, 2, 3 ou 9");
+                break;
+        }
     }
-
-    
-
-
 }
--- src/main/java/fr/houseofcode/dap/commandLine/rma/UtilsServer.java
+++ src/main/java/fr/houseofcode/dap/commandLine/rma/UtilsServer.java
@@ -1,5 +1,8 @@
 package fr.houseofcode.dap.commandLine.rma;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,32 +19,52 @@ import java.net.URL;
  * to google API
  */
 public class UtilsServer {
-
-    /**constant to define which browse is using.*/
-    private  String uSErAGENT = "Mozilla/5.0";
+    /**
+     * @return access to constant LOG.
+     */
+    private static final Logger LOG = LogManager.getLogger();
+    /**Constant to define which browse is using.*/
+    private  String userAgent = "Mozilla/5.0";
 
     /**
-     * load the next event on server.
+     * Load web page to add a user account on server.
      * @param userKey accept name of person who use the application
-     * @return the next event on server using identification
      * @throws IOException exception
      */
-    
-    public void createAccount (String userKey) throws IOException {
-    	String account = "http://localhost:8080/account/add/" + userKey;
-    	try {
-			Desktop.getDesktop().browse(new URI(account));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public void addUser(final String userKey) throws IOException {
+        String addUser = "http://localhost:9090/user/add?name=" + userKey;
+        loadWebPage(addUser);
     }
-    
-    
-    public String getNextEvent( String userKey) throws IOException {
+
+    /**
+     * Load web page to create a Google account on server.
+     * @param userKey accept name of person who use the application
+     * @throws IOException exception
+     */
+    public void createAccount(final String userKey) throws IOException {
+        String account = "http://localhost:9090/account/add/" + userKey;
+        loadWebPage(account);
+    }
+
+    private void loadWebPage(final String account) {
+        try {
+            Desktop.getDesktop().browse(new URI(account));
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            LOG.error(e.getMessage(), e);
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Load event on server.
+     * @param userKey
+     * @return a String that describe the event
+     * @throws IOException
+     */
+    public String getNextEvent(final String userKey) throws IOException {
         String event = callServer("/event/next", userKey);
         return event;
     }
@@ -52,7 +75,7 @@ public class UtilsServer {
      * @return the labels on server using identification
      * @throws IOException exception
      */
-    public String getLabels(String userKey) throws IOException {
+    public String getLabels(final String userKey) throws IOException {
         String label = callServer("/label/print", userKey);
         return label;
     }
@@ -63,13 +86,10 @@ public class UtilsServer {
      * @return the number of unread email on server using identification
      * @throws IOException exception
      */
-    public String getNbUnreadEmail( String userKey) throws IOException {
+    public String getNbUnreadEmail(final String userKey) throws IOException {
         String nbEmail = callServer("/email/nbUnread", userKey);
-
         return nbEmail;
     }
-
-
 
     /**
      * call the server.
@@ -78,25 +98,21 @@ public class UtilsServer {
      * @return a call to the server using identification
      * @throws IOException exception
      */
-    private String callServer( String url,  String userKey)
+    private String callServer(final String url, final String userKey)
             throws IOException {
-
-    		
-        URL obj = new URL("http://localhost:8080"
+        URL obj = new URL("http://localhost:9090"
                 + url + "?userKey=" + userKey);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-
-    	
         // optional default is GET
         con.setRequestMethod("GET");
 
         //add request header
-        con.setRequestProperty("User-Agent", uSErAGENT);
-    	
+        con.setRequestProperty("User-Agent", userAgent);
+
         int responseCode = con.getResponseCode();
         System.out.println("\nSending 'GET' request to URL : "
-                + "http://localhost:8080" + url);
+                + "http://localhost:9090" + url);
 
         System.out.println("Response Code : " + responseCode);
 
@@ -104,15 +120,14 @@ public class UtilsServer {
                 new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuffer response = new StringBuffer();
-    	
+
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
         }
         in.close();
-    	
+
         //print result
         return response.toString();
-
     }
 
 }
